debc5d0c9e00e8a044354c286c8d6ddff15bb9d2
Hugues McNeil-Duval <47720660+huguesmcneilduval@users.noreply.github.com>
2026-03-25 14:24:53 -0400

    improvement/support having pipeline config file in project (#6)

--- src/com/duvalhub/deploy/parameters/Parameters.groovy
+++ src/com/duvalhub/deploy/parameters/Parameters.groovy
@@ -5,12 +5,14 @@ class Parameters {
     String git_repository
     String environment
     String version
+    String label
     String configGitBranch
 
-    Parameters(String git_repository, String environment, String version, String configGitBranch) {
+    Parameters(String git_repository, String environment, String version, String label, String configGitBranch) {
         this.git_repository = git_repository
         this.environment = environment
         this.version = version
+        this.label = label
         this.configGitBranch = configGitBranch
     }
 }
